5b0cfdf25e3ae4fad87941c0c22c040df5b54baa
mahamadou-simpara <msimpara@doninya.com>
2023-07-24 23:58:07 +0000

    jokes genetor done 2

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ const btnEl = document.getElementById("btn");
 
 generateJokes();
 
-// btnEl.addEventListener("click", generateJokes);
+btnEl.addEventListener("click", generateJokes);
 
-// async function generateJokes() {
-//   const header = {
-//     headers: {
-//       "Accept": "application/json",
-//     },
-//   };
+async function generateJokes() {
+  const header = {
+    headers: {
+      "Accept": "application/json",
+    },
+  };
 
-//   const result = await fetch("https://icanhazdadjoke.com", header);
+  const result = await fetch("https://icanhazdadjoke.com", header);
 
-//   console.log(result);
+  console.log(result);
 
-//   const data = await result.json();
+  const data = await result.json();
 
-//   console.log(data);
-//   jokeEl.innerHTML = data.joke;
-// }
+  console.log(data);
+  jokeEl.innerHTML = data.joke;
+}
 
